021a42d41dadfd4b2b047d8542a8e1c71b10440b
Kobby Adu <kobby129@gmail.com>
2019-10-08 10:05:50 -0600

    actual finished bigger_two code

--- list_quiz.rb
+++ list_quiz.rb
@@ -13,8 +13,22 @@ def three_even(list)
     end
 end
 
-puts three_even([2,1,3,5])
-puts three_even([2,4,12,5])
-puts three_even([2,1,4,6])
-puts three_even([1,4,6,4])
-puts three_even([])
+# puts three_even([2,1,3,5])
+# puts three_even([2,4,12,5])
+# puts three_even([2,1,4,6])
+# puts three_even([1,4,6,4])
+# puts three_even([])
+
+def bigger_two(list1,list2)
+    i = 0
+    list1_value = (list1[i] + list1[i+1])
+    list2value =  (list2[i] + list2[i+1])
+    if list1_value>= list2value
+        return list1
+    else 
+        return list2 
+    end
+
+end
+
+puts bigger_two([1,2] , [3,4])
